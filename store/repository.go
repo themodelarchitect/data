18139d73a6eaabfdec360dabcb3ecbe1daff331d
Jonathan Kittell <jkittell@gmail.com>
2023-12-17 12:27:58 -0500

    file repo

--- store/repository.go
+++ store/repository.go
@@ -1,18 +1,9 @@
 package store
 
-import (
-	"github.com/jkittell/data/structures/array"
-)
-
-type Repository[T Entity] interface {
+type Repository[T any] interface {
 	Create(entity T) (int, error)
 	Update(id int, entity T) error
 	Get(id int) (T, error)
-	GetAll() *array.Array[T]
+	GetAll() map[int]T
 	Delete(id int) error
-}
-
-type Entity interface {
-	GetID() int
-	SetID(id int)
 }
--- structures/array.go
+++ structures/array.go
@@ -1,4 +1,4 @@
-package array
+package structures
 
 import (
 	"encoding/json"
